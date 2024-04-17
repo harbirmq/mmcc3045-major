7f49a99eee956a76aab6b548b0b9a52860d5a4e2
harbirmq <100890075+harbirmq@users.noreply.github.com>
2024-04-17 14:44:33 +1000

    add buffs library

---
+++ libraries/buffs_library.js
@@ -0,0 +1,20 @@
+// type
+function Buff(_name, _description, stats) {
+	return {
+		description: _description,
+		stats: stats,
+		storage: {
+			name: _name,
+		}
+	};
+}
+
+// library
+const BUFFS = {
+	"Missing Eye": Buff("Missing Eye", "Your right eye is missing...", { perception: -1 }),
+};
+
+// functions
+function AddBuff(buffID) { AddGeneric(buffID, buffs, "buffs"); }
+function RemoveBuff(buffID) { RemoveGeneric(buffID, buffs, "buffs"); }
+function HasBuff(buffID) { HasGeneric(buffID, buffs); }
